4e654c679cc10edf74038cc8ed164fba9fdbe7b3
Cloris Li <cloris.lzy@gmail.com>
2023-03-13 07:47:07 -0700

    add detailed route checklist

--- routes/results.js
+++ routes/results.js
@@ -1,15 +1,26 @@
 /*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into /users,
- *   these routes are mounted onto /users
+ * All routes for results are defined here
+ * Since this file is loaded in server.js into /results,
+ *   these routes are mounted onto /results
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('results');
+router.get('/:id', (req, res) => {
+  //  const id = req.params.id;
+
+  // if results database contains a result with the given id, procceed
+    // const result = getResultById(id, resultsDatabase);
+  // if the result id is not found, return alert message
+
+  // res.render('results', result);
 });
+
+function getResultById (id, resultsDatabase) {
+  // find the result in the resultsDatabase that matches the given id
+  // return the result
+}
 
 module.exports = router;
