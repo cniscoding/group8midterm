1e238abd489e2e9cde189e462f61b7d80f1d8cb5
cniscoding <asdfa.com>
2023-03-15 15:59:20 -0700

    update database and header partial

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,4 +1,8 @@
-INSERT INTO quizzes (user_id, title, public) VALUES (1,'Math','public');
-INSERT INTO quizzes (user_id, title, public) VALUES (1,'Cartoons','public');
+INSERT INTO quizzes (user_id, title, public) VALUES (1,'Math Quiz','public');
+INSERT INTO quizzes (user_id, title, public) VALUES (1,'Cartoons Quiz','public');
 INSERT INTO quizzes (user_id, title, public) VALUES (1, 'Fun Trivia Quiz', 'private');
-INSERT INTO quizzes (user_id, title, public) VALUES (1, 'TV', 'private');
+INSERT INTO quizzes (user_id, title, public) VALUES (1, 'TV Quiz', 'public');
+INSERT INTO quizzes (user_id, title, public) VALUES (1, 'Real Housewives Quiz', 'private');
+INSERT INTO quizzes (user_id, title, public) VALUES (1, 'Dog Quiz', 'public');
+INSERT INTO quizzes (user_id, title, public) VALUES (1, 'US Politics Quiz', 'public');
+INSERT INTO quizzes (user_id, title, public) VALUES (1, 'Basic Geography Quiz', 'private');
